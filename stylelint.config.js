be86bcc070c27651ce60dff0ee78e6a693492ddf
Aleksandr what3vergl Nokhrin <nokhrin.alni@gmail.com>
2024-07-09 00:27:07 +0500

    Migrate existing css to scss. Configure stylelint to handle scss files

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,33 @@
 export default {
     extends: [
         "stylelint-config-recommended",
-        "stylelint-config-tailwindcss"
+        "stylelint-config-standard",
     ],
+    overrides: [
+        {
+            files: ["**/*.scss"],
+            customSyntax: "postcss-scss"
+        }
+    ],
+    rules: {
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: [
+                    'tailwind',
+                    'apply',
+                    'variants',
+                    'responsive',
+                    'screen',
+                ],
+            },
+        ],
+        'function-no-unknown': [
+            true,
+            {
+                ignoreFunctions: ['theme'],
+            },
+        ],
+        'no-descending-specificity': null,
+    }
 }
