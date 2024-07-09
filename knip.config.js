1967dcb1c2230232ecd1813b3a2f920677fda74d
Aleksandr what3vergl Nokhrin <nokhrin.alni@gmail.com>
2024-07-09 14:01:54 +0500

    Apply rules for knip config

--- knip.config.js
+++ knip.config.js
@@ -1,5 +1,22 @@
 /** @type {import('knip').KnipConfig} */
 export default {
+    includeEntryExports: true,
+    rules: {
+        files: "warn", // cannot ignore unused files which might exist as precursor work
+        dependencies: "error",
+        devDependencies: "error",
+        optionalPeerDependencies: "error",
+        unlisted: "error",
+        binaries: "error",
+        unresolved: "error",
+        exports: "warn", // still open issues to squash
+        types: "warn", // still open issues to squash
+        nsExports: "off", // off by default
+        nsTypes: "off", // off by default
+        enumMembers: "off", // many open issues still; some might not be resolvable without changes to knip
+        classMembers: "off", // off by default -- consumes a lot of memory and may cause crashes
+        duplicates: "error",
+    },
     workspaces: {
         "projects/server": {
             entry: ["src/main.tsx"],
